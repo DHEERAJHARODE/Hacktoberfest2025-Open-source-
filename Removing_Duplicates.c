efb3293fd44ffba677b8d10e969bd6aef2b32665
aaryab27 <aaryabhadale27@gmail.com>
2025-10-20 18:04:22 +0530

    added the leetcode soulution to the problem removing duplicates from a sorted array

---
+++ Removing_Duplicates.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int removeDuplicates(int* nums, int numsSize)
+ {
+    if(numsSize==0)
+    {
+        return 0;
+    }
+   int i=0;
+   for(int j=1;j<numsSize;j++)
+   {
+    if(nums[j]!=nums[i])
+    {
+        i++;
+        nums[i]=nums[j];
+    }
+   }
+   return i+1;
+   }
+   int main()
+   {
+int arr[]={1,2,2,3,4};
+printf("New Size of Array is:- %d",removeDuplicates(arr,5));
+return 0;
+}
